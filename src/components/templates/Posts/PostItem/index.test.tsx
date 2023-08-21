b4497cc6799492af23287e9f7e1fe84a3c327683
Junya <junya.ishibuchi@gmail.com>
2023-08-21 10:53:46 -0700

    fix: commonize setup process

--- src/components/templates/Posts/PostItem/index.test.tsx
+++ src/components/templates/Posts/PostItem/index.test.tsx
@@ -1,19 +1,25 @@
 import {PostItem} from "@/components/templates/Posts/PostItem/index";
 import {getPostsData} from "@/services/server/Posts/__mock__/fixture";
-import {render, screen} from "@testing-library/react";
+import {render} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import mockRouter from "next-router-mock";
 
-test("An accessible name is the title of the input post", () => {
+function setup() {
   const post = getPostsData.posts[0];
-  render(<PostItem post={post} />);
-  const link = screen.getByRole('link');
+  const postItem = render(<PostItem post={post} />);
+  const link = postItem.getByRole('link');
+  const click = () => userEvent.click(link);
+
+  return { post, link, click }
+}
+
+test("An accessible name is the title of the input post", () => {
+  const { post, link } = setup();
   expect(link).toHaveAccessibleName(post.title);
 });
 
 test("when you click the link, you can move to the link", async () => {
-  render(<PostItem post={getPostsData.posts[0]} />);
-  const link = screen.getByRole('link');
-  await userEvent.click(link);
-  expect(mockRouter).toMatchObject({pathname: '/posts/1'});
+  const { post,  click } = setup();
+  await click();
+  expect(mockRouter).toMatchObject({pathname: `/posts/${post.id}`});
 });

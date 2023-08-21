02ed455475628675e6a61481d81bc114ef5e0cbf
Junya <junya.ishibuchi@gmail.com>
2023-08-21 11:32:15 -0700

    fix: indent

--- src/components/templates/Posts/Header/OrderBy/index.test.tsx
+++ src/components/templates/Posts/Header/OrderBy/index.test.tsx
@@ -6,17 +6,17 @@ import mockRouter from "next-router-mock";
 const user = userEvent.setup();
 
 const setup = () => {
-    render(<OrderBy/>);
-    return screen.getByRole("combobox");
+  render(<OrderBy/>);
+  return screen.getByRole("combobox");
 }
 
 test("default value is 更新日時順", () => {
-    const list = setup();
-    expect(list).toHaveDisplayValue("更新日時順");
+  const list = setup();
+  expect(list).toHaveDisplayValue("更新日時順");
 });
 
 test("If you change a option, query is going to change the value you choose", async () => {
-    const list = setup();
-    await user.selectOptions(list, "スター数順");
-    expect(mockRouter).toMatchObject({ query: { orderBy: "starCount" } });
+  const list = setup();
+  await user.selectOptions(list, "スター数順");
+  expect(mockRouter).toMatchObject({ query: { orderBy: "starCount" } });
 });

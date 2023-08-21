70e4aee4f1f824297884d6fdc5c4dd4205fb3137
Junya <junya.ishibuchi@gmail.com>
2023-08-21 10:27:22 -0700

    feat: implement testing about accessible name

--- src/components/templates/Posts/PostItem/index.test.tsx
+++ src/components/templates/Posts/PostItem/index.test.tsx
@@ -1,8 +1,15 @@
 import {PostItem} from "@/components/templates/Posts/PostItem/index";
 import {getPostsData} from "@/services/server/Posts/__mock__/fixture";
-import {getByRole, render, screen} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import mockRouter from "next-router-mock";
+
+test("An accessible name is the title of the input post", () => {
+  const post = getPostsData.posts[0];
+  render(<PostItem post={post} />);
+  const link = screen.getByRole('link');
+  expect(link).toHaveAccessibleName(post.title);
+});
 
 test("when you click the link, you can move to the link", async () => {
   render(<PostItem post={getPostsData.posts[0]} />);

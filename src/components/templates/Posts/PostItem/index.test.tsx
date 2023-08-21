9b9240e252c463affee181b28a2853b5b57bfaa8
Junya <junya.ishibuchi@gmail.com>
2023-08-21 11:00:10 -0700

    fix: saw the answer and wrote that down

--- src/components/templates/Posts/PostItem/index.test.tsx
+++ src/components/templates/Posts/PostItem/index.test.tsx
@@ -1,14 +1,16 @@
 import {PostItem} from "@/components/templates/Posts/PostItem/index";
 import {getPostsData} from "@/services/server/Posts/__mock__/fixture";
-import {render} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import mockRouter from "next-router-mock";
 
-function setup() {
+const user = userEvent.setup();
+
+const setup =  () => {
   const post = getPostsData.posts[0];
-  const postItem = render(<PostItem post={post} />);
-  const link = postItem.getByRole('link');
-  const click = () => userEvent.click(link);
+  render(<PostItem post={post} />);
+  const link = screen.getByRole('link');
+  const click = () => user.click(link);
 
   return { post, link, click }
 }
@@ -19,7 +21,7 @@ test("An accessible name is the title of the input post", () => {
 });
 
 test("when you click the link, you can move to the link", async () => {
-  const { post,  click } = setup();
+  const { post, click } = setup();
   await click();
-  expect(mockRouter).toMatchObject({pathname: `/posts/${post.id}`});
+  expect(mockRouter).toMatchObject({ pathname: `/posts/${post.id}` });
 });

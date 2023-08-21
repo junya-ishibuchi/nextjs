caeb0076f7851e4a01bbdb2e3cf2c3fefc144064
Junya <junya.ishibuchi@gmail.com>
2023-08-21 11:21:41 -0700

    feat: implement testing

---
+++ src/components/templates/Posts/Header/OrderBy/index.test.tsx
@@ -0,0 +1,22 @@
+import {OrderBy} from "@/components/templates/Posts/Header/OrderBy/index";
+import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import mockRouter from "next-router-mock";
+
+const user = userEvent.setup();
+
+const setup = () => {
+    render(<OrderBy/>);
+    return screen.getByRole("combobox");
+}
+
+test("default value is 更新日時順", () => {
+    const list = setup();
+    expect(list).toHaveDisplayValue("更新日時順");
+});
+
+test("If you change a option, query is going to change the value you choose", async () => {
+    const list = setup();
+    await user.selectOptions(list, "スター数順");
+    expect(mockRouter).toMatchObject({ query: { orderBy: "starCount" } });
+});

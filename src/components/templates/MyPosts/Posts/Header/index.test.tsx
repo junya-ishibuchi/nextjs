82dceb90098fb550958ab9deb40007ec6adafdd6
Junya <junya.ishibuchi@gmail.com>
2023-08-25 10:24:57 -0700

    refactor: compare to the original one

--- src/components/templates/MyPosts/Posts/Header/index.test.tsx
+++ src/components/templates/MyPosts/Posts/Header/index.test.tsx
@@ -9,9 +9,31 @@ test.each([
   { value: "all", label: "すべて" },
   { value: "public", label: "公開" },
   { value: "private", label: "下書き" }
+])("When a url has status query '$value', an option display '$value'", (option) => {
+  mockRouter.setCurrentUrl(`my/posts?status=${option.value}`)
+  render(<Header />);
+
+  expect(screen.getByRole("combobox")).toHaveDisplayValue(option.label);
+});
+
+test.each([
+  { value: "all", label: "すべて" },
+  { value: "public", label: "公開" },
+  { value: "private", label: "下書き" }
 ])("When you select $value, a query is going to be changed", async (option) => {
   render(<Header />);
   await user.selectOptions(screen.getByRole("combobox"), option.value);
 
   expect(mockRouter).toMatchObject({ query: { status: option.value }});
+});
+
+test("When you select $value, the originally set query is not deleted.", async () => {
+  mockRouter.setCurrentUrl(`my/posts?page=1`)
+  render(<Header />);
+
+  await user.selectOptions(screen.getByRole("combobox"), "all");
+  expect(mockRouter).toMatchObject({ query: { status: "all", page: "1" }});
+
+  await user.selectOptions(screen.getByRole("combobox"), "public");
+  expect(mockRouter).toMatchObject({ query: { status: "public", page: "1" }});
 });

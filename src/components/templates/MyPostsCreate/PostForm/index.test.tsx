42dd2535a0dbcd0f82e8ee2b6bcb93aaa3f0b92a
Junya <junya.ishibuchi@gmail.com>
2023-08-28 15:54:30 -0700

    refactor: compare to the original one

--- src/components/templates/MyPostsCreate/PostForm/index.test.tsx
+++ src/components/templates/MyPostsCreate/PostForm/index.test.tsx
@@ -1,4 +1,4 @@
-import {render, screen} from "@testing-library/react";
+import {render, screen, waitFor} from "@testing-library/react";
 import {PostForm} from "@/components/templates/MyPostsCreate/PostForm/index";
 import userEvent from "@testing-library/user-event";
 import {selectImageFile, setupMockServer} from "@/tests/jest";
@@ -23,23 +23,30 @@ const setUp = (title: string = "title") => {
   const typeTitle = async (title: string) => {
     await user.type(screen.getByRole("textbox", { name: "記事タイトル" }), title);
   }
-  const clickSaveButton = async () => {
+  const saveAsDraft = async () => {
+    await user.click(screen.getByRole("button", { name: "下書き保存する" }));
+  }
+  const saveAsPublish = async () => {
     await user.click(screen.getByRole("button", { name: "下書き保存する" }));
   }
 
-  return { onClickSave, onInvalid, onValid, typeTitle, clickSaveButton };
+  return { onClickSave, onInvalid, onValid, typeTitle, saveAsDraft, saveAsPublish };
 };
 
-test("The form has a label", () => {
-  setUp();
-  expect(screen.getByRole("form")).toHaveAccessibleName("title");
+test("When you just save it, a validation error text shows", async () => {
+  const { saveAsDraft } = setUp();
+  await saveAsDraft();
+
+  await waitFor(() => {
+    expect(screen.getByRole("textbox", { name: "記事タイトル" })).toHaveErrorMessage("1文字以上入力してください");
+  });
 })
 
 test("When you only input a title, a validation error occurs", async () => {
-  const { onClickSave, onInvalid, onValid, typeTitle, clickSaveButton } = setUp();
+  const { onClickSave, onInvalid, onValid, typeTitle, saveAsDraft } = setUp();
 
   await typeTitle("input title");
-  await clickSaveButton();
+  await saveAsDraft();
 
   expect(onClickSave).toHaveBeenCalled();
   expect(onInvalid).toHaveBeenCalled();
@@ -48,11 +55,11 @@ test("When you only input a title, a validation error occurs", async () => {
 
 test("When you only input a image file, a validation error occurs", async () => {
   mockUploadImage();
-  const { onClickSave, onInvalid, onValid, clickSaveButton } = setUp();
+  const { onClickSave, onInvalid, onValid, saveAsDraft } = setUp();
   const { selectImage } = selectImageFile();
 
   await selectImage();
-  await clickSaveButton();
+  await saveAsDraft();
 
   expect(onClickSave).toHaveBeenCalled();
   expect(onInvalid).toHaveBeenCalled();
@@ -61,23 +68,28 @@ test("When you only input a image file, a validation error occurs", async () => 
 
 test("The form works with valid values when you save as a draft", async () => {
   mockUploadImage();
-  const { onClickSave, onInvalid, onValid, typeTitle, clickSaveButton } = setUp();
+  const { onClickSave, onInvalid, onValid, typeTitle, saveAsDraft } = setUp();
   const { selectImage } = selectImageFile();
 
   await typeTitle("input title");
   await selectImage();
-  await clickSaveButton();
+  await saveAsDraft();
 
   expect(onClickSave).toHaveBeenCalled();
   expect(onInvalid).not.toHaveBeenCalled();
   expect(onValid).toHaveBeenCalled();
 })
 
-test("You can toggle the status", async () => {
-  setUp();
+test("When you publish a post, invalid and valid methods is not going to be called", async () => {
+  mockUploadImage();
+  const { onClickSave, onInvalid, onValid, typeTitle, saveAsPublish } = setUp();
+  const { selectImage } = selectImageFile();
 
-  expect(screen.queryByRole("button", { name: "記事を公開する" })).not.toBeInTheDocument();
-  await user.click(screen.getByRole("switch", { name: "公開ステータス" }));
+  await typeTitle("input title");
+  await selectImage();
+  await saveAsPublish();
 
-  expect(screen.getByRole("button", { name: "記事を公開する" })).toBeInTheDocument();
+  expect(onClickSave).toHaveBeenCalled();
+  expect(onInvalid).not.toHaveBeenCalled();
+  expect(onValid).not.toHaveBeenCalled();
 })
